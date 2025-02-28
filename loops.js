f9deb3888a8042928d9c3128b127382ffc548cd8
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-28 14:49:58 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,16 @@
+//for loop
+for(let i=1;i<=5;i++){
+    console.log("number:"+i);
+}
+
+//while loop
+let i=1;
+while(i<=4){
+    console.log("count:"+i);
+    i++;
+}//do-while loop
+let j=1;
+do{
+    console.log("Do-while count:"+j);
+    j++;
+}while(j<=3);
