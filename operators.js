268efe3a6b64ecc283f8908378c273bf3892b7b9
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-28 14:49:40 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 5;
+
+console.log(a + b);  // Addition -> 15
+console.log(a - b);  // Subtraction -> 5
+console.log(a * b);  // Multiplication -> 50
+console.log(a / b);  // Division -> 2
+console.log(a % b);  // Modulus -> 0
+console.log(a ** b); // Exponentiation -> 10^5 = 100000
+
+console.log(10 > 5);   // true
+console.log(10 < 5);   // false
+console.log(10 == "10");  // true (checks only value)
+console.log(10 === "10"); // false (checks value and type)
+console.log(10 !== "10"); // true
