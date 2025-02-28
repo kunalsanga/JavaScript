e42f7f2bddc2f8ffdab5fa1e4d6f2f8daafc87ab
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-28 14:49:27 +0530

    Add files via upload

---
+++ variable-and-datatypes.js
@@ -0,0 +1,12 @@
+//Variables and Data Types
+//JavaScript has three ways to declare variables:
+//var (old, avoid using)
+//let (block-scoped, recommended)
+//const (cannot be reassigned)
+let name = "Alice";//string
+const age=25;//number
+let isStudent=true;//boolean
+let skills = ["JS","Python","C++"];//array
+let person={name: "John", age:30};//object
+
+console.log(name,age,isStudent,skills,person);
