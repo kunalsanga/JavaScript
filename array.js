f24ab3c4b8c2d499418d48f9d585b4c15d7eeedb
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-28 14:50:47 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+let fruits=["apple","banana","mango"];
+console.log(fruits.length);
+console.log(fruits[1]);
+fruits.push("orange");//add at end
+console.log(fruits);
+fruits.pop();//remove the last element
+console.log(fruits);
+fruits.unshift("kiwi");//add at start
+console.log(fruits);
+fruits.shift();//remove the first element
+console.log(fruits);
