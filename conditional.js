97c3a99551049a2361b69f625591515bebb7782e
kunal sanga <152892694+kunalsanga@users.noreply.github.com>
2025-02-28 14:50:34 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,12 @@
+let num=10;
+if(num>0){
+    console.log("positive number");
+}else if(num<0){
+    console.log("negative number");
+}else{
+    console.log("zero");
+}
+
+//ternary operator
+let result = (num>0)? "positive":"not positive";
+console.log(result);
